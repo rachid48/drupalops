eea2783aa8fd3ebc97e3fe1b20a0e7b4e953d892
rachid <rachidtouhami90@gmail.com>
2026-09-05 17:35:39 +0200

    fix: use current AWS state bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "drupalops-tfstate-133197901339"
+    bucket         = "drupalops-tfstate-408396500844"
     key            = "drupalops/terraform.tfstate"
     region         = "eu-west-3"
     dynamodb_table = "drupalops-tfstate-lock"
